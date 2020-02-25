a1ccad455689f38d744e43c7cf93447e283988a0
William Douglas Smith <129franklin@sbcglobal.net>
2020-02-24 16:38:30 -0800

    revised schema.sql file.

--- schema.sql
+++ schema.sql
@@ -32,14 +32,14 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-CREATE TABLE dept_employees (
+CREATE TABLE dept_emp (
 	 emp_no INT NOT NULL,
      dept_no VARCHAR(4) NOT NULL,
      from_date DATE NOT NULL,
      to_date DATE NOT NULL,
 	 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-     PRIMARY KEY (emp_no)
+     PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE titles (
   emp_no INT NOT NULL,
@@ -47,5 +47,5 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, from_date)
 );
